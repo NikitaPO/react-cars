f73f26cd1fa04767825ead0782140a5c82efeec9
NikitaPO <gigzim345@gmai.com>
2020-02-01 17:34:26 +0300

    before eject

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "./Car.css";
 
-function Car(props) {
-  return (
-    <li className="car">
-      <h3 className="car__title">{props.name}</h3>
-      <p>
-        Car price: <strong>{props.price}$</strong>
-      </p>
-      <p>
-        Car year: <strong>{props.year}</strong>
-      </p>
-      <button className="close" onClick={props.onDelete}></button>
-    </li>
-  );
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const { props } = this;
+
+    return (
+      <li className="car">
+        <h3 className="car__title">{props.name}</h3>
+        <p>
+          Car price: <strong>{props.price}$</strong>
+        </p>
+        <p>
+          Car year: <strong>{props.year}</strong>
+        </p>
+        <button className="close" onClick={props.onDelete}></button>
+      </li>
+    );
+  }
 }
 
 export default Car;
